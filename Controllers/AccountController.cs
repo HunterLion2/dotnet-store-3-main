4cf062875fa434272a93e93dce700e4050a24a52
Çınar Özgürgil <cozgurgil@gmail.com>
2025-05-05 21:05:59 +0300

    Lockout-Settings

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using dotnet_store.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -54,19 +55,40 @@ public class AccountController : Controller
     }
 
     [HttpPost]
-    public async Task<ActionResult> LoginAsync(AccountLoginModel model) {
+    public async Task<ActionResult> Login(AccountLoginModel model, string? returnUrl) {
         if(ModelState.IsValid) {
             var user = await _userManager.FindByEmailAsync(model.Email);
             
             if(user != null) {
 
+                // _signInManager değeri ile uygulamaya giriş ve çıkışları kontrol ederiz
                 await _signInManager.SignOutAsync();
-
-                var result = await _signInManager.PasswordSignInAsync(user, model.Password,model.BeniHatirla,false);
+ 
+                // PasswordSignInAsync değeri ile bu değerin içine girilen değerler datadakiler uyumuna bakılır.
+                var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.BeniHatirla, true);
 
                 if(result.Succeeded) {
-                    return RedirectToAction("Index","Home");
-                } else {
+
+                    // Buradaki ikilinin ilki ile giriş yaparkenki fail bilgilerini silmiş oluruz. 
+                    await _userManager.ResetAccessFailedCountAsync(user);
+                    await _userManager.SetLockoutEndDateAsync(user, null);
+
+                    // ---------------------------------------------
+
+                    if(!string.IsNullOrEmpty(returnUrl)) {
+                        return Redirect(returnUrl);
+                    } else {
+                        return RedirectToAction("Index","Home");
+                    }
+
+                } else if(result.IsLockedOut) {
+                    // GetLockoutEndDateAsync(user) bilgisi kullanıcının giriş için ne kadar sonra hesabının kitlendiği bilgisini bize söyler
+                    var locoutDate = await _userManager.GetLockoutEndDateAsync(user);
+                    var timeLeft = locoutDate.Value - DateTime.UtcNow;
+
+                    ModelState.AddModelError("",$"Hesabınız Kitlenmiştir.Lütfen {timeLeft.Minutes + 1} dakika sonra tekrar deneyiniz.");
+                }
+                else {
                     ModelState.AddModelError("","Hatalı Parola");
                 }
 
@@ -76,6 +98,16 @@ public class AccountController : Controller
         }
 
         return View(model);
+    }
+
+    public async Task<ActionResult> Logout() {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Login","Account");
+    }
+
+    [Authorize]
+    public ActionResult Settings() {
+        return View();
     }
 
 }
