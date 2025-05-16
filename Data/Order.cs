ffd6fff9fde3e21b6d33a718a471c3740c1096e0
Çınar Özgürgil <cozgurgil@gmail.com>
2025-05-16 12:59:09 +0300

    Order-Tables

--- Data/Order.cs
+++ Data/Order.cs
@@ -4,7 +4,7 @@ namespace dotnet_store.Models;
 
 public class Order
 {
-    public int SiparisId { get; set; }
+    public int Id { get; set; }
 
     public DateTime SiparisTarihi { get; set; }
 
@@ -21,10 +21,24 @@ public class Order
     public string Email { get; set; } = null!;
 
     public double ToplamFiyat { get; set; }
+
+    public List<OrderItem> OrderItems { get; set; } = new();
 }
 
 public class OrderItem
 {
-    public int OrderItemId { get; set; }
+    public int Id { get; set; }
+
+    public int OrderId { get; set; }
+
+    public Order Order { get; set; } = null!;
+
+    public int UrunId { get; set; }
+
+    public Urun Urun { get; set; } = null!;
+
+    public double Fiyat { get; set; }
+
+    public int Miktar { get; set; }
 }
 
